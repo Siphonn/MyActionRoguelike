755e6b1fd29eb1385b7a8746a2da1315787469fe
FionnS <fionnsherrard@gmail.com>
2023-01-28 21:55:07 +0000

    Lecture 12 - More AI, Environment Query Spawn Logic
    EQS to find Bot spawn points
    GameMode with custom AI spawn behaviour

--- Source/MyActionRoguelike/Private/AI/SAICharacter.cpp
+++ Source/MyActionRoguelike/Private/AI/SAICharacter.cpp
@@ -11,6 +11,8 @@ ASAICharacter::ASAICharacter()
 {
 	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>("PawnSensingComp");
 	AttributeComp = CreateDefaultSubobject<USAttributeComponent>("AttributeComp");
+
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
 }
 
 void ASAICharacter::PostInitializeComponents()
