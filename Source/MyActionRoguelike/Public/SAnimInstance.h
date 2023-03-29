7235504dbd82985fb03c3f1e9cc7cc735fde60d8
FionnS <fionnsherrard@gmail.com>
2023-03-29 22:02:54 +0100

    Lecture 26 – Animation Blueprints & UI Improvements
    - Animation State Machines (Stunned Effect) & C++ AnimInstance
    - Blend Poses & Blend Spaces (Floating Sprint Animation)

---
+++ Source/MyActionRoguelike/Public/SAnimInstance.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SAnimInstance.generated.h"
+
+class USActionComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class MYACTIONROGUELIKE_API USAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+protected:
+	/* Is Pawn Stunned based on GameplayTag data */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Animation")
+	bool bIsStunned;
+	UPROPERTY(BlueprintReadOnly, Category="Animation")
+	USActionComponent* ActionComp;
+
+	virtual void NativeInitializeAnimation() override;
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+};
